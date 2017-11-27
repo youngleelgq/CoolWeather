4271bb3ee5dcfdc7313d24cbf66d0f2be4171903
LiGQ(李贵强/深圳) <ligq@paxsz.local>
2017-11-27 10:49:54 +0800

    create project:first commit

---
+++ app/src/main/java/com/pax/coolweather/MainActivity.java
@@ -0,0 +1,16 @@
+package com.pax.coolweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * @author ligq
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
